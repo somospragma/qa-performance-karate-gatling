fb022f0bfb63fcd836c488f1573311f984e8e12a
CristianMillanPragma <135721691+CristianMillanPragma@users.noreply.github.com>
2024-12-17 20:39:33 -0500

    Delete firebase_lib_test directory

--- firebase_lib_test/src/main/java/co/com/bancolombia/certification/svn/application/strategy/KarateFirebaseImplementation.java
+++
@@ -1,9 +0,0 @@
-package co.com.bancolombia.certification.svn.application.strategy;
-
-/***
- * Implementation class to use in karate automation test projects
- */
-public class KarateFirebaseImplementation {
-
-    //
-}
--- firebase_lib_test/src/main/java/co/com/bancolombia/certification/svn/factory/firestore/ObjectConnection.java
+++
@@ -1,8 +0,0 @@
-package co.com.bancolombia.certification.svn.factory.firestore;
-
-import com.google.cloud.firestore.Firestore;
-
-public interface ObjectConnection {
-
-    public Firestore getFirestore();
-}
