368f740c7bcf02f420414e999bbcc948280a778c
CristianMillanPragma <135721691+CristianMillanPragma@users.noreply.github.com>
2025-03-05 08:07:29 -0500

    Delete performance-test_gatling directory

--- performance-test_gatling/src/test/java/utils/UuidFormat.java
+++
@@ -1,9 +0,0 @@
-package utils;
-import java.util.UUID;
-
-public class UuidFormat {
-    public String formatMessage() {
-        UUID uuid = UUID.randomUUID();
-        return uuid.toString();
-    }
-}
